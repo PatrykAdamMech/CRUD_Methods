8fd4bb285c02b616ed2bbffa54cd52e7a9fe30a1
Patryk Mech <PatrykAdamMech@gmail.com>
2022-02-24 09:25:15 +0100

    Query Params Examples

--- src/main/java/com/patryk/firstworkingproject/HelloResource.java
+++ src/main/java/com/patryk/firstworkingproject/HelloResource.java
@@ -3,12 +3,18 @@ package com.patryk.firstworkingproject;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
 
 @Path("/hello-world")
 public class HelloResource {
     @GET
     @Produces("text/plain")
+    public String hello(@QueryParam("name") String name) {
+        return "Hello, "+name;
+    }
+    @GET
+    @Produces("text/plain")
     public String hello() {
-        return "Hello, World!";
+        return "Hello, world";
     }
 }
